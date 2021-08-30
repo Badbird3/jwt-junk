9516e20dcdf27a8349560be1dec6e6971e84afd7
Tommy Fields <tfields@netspi.com>
2021-08-30 15:28:10 -0700

    added

---
+++ epoch copy.py
@@ -0,0 +1,4 @@
+import time 
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370)))
+print('test')
---
+++ epoch.py
@@ -0,0 +1,6 @@
+import datetime
+
+epoch_time = 1630362893
+datetime_time = datetime.datetime.fromtimestamp(epoch_time)
+
+print(datetime_time)
