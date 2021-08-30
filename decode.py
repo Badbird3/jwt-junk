d822570cf1a42c2f483548c80b2da8cf079c60bf
Tommy Fields <tfields@netspi.com>
2021-08-30 15:37:50 -0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,13 +1,20 @@
 import jwt
+import argparse
 
-# todo, token token as cli input, see awss for a good module for that
+# todo, token token as cli input, use argeparse
 
-token = "foo"
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('jwt', metavar='J', type=str, nargs='+',
+                    help='enter a jwt')
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
+#token = "foo"
 
 # todo
 # get the 'exp' parameter and print it, then check to see how long until it expires
 
-decoded = jwt.decode(token, options={"verify_signature": False}) # works in PyJWT >= v2.0
-print (decoded)
+#decoded = jwt.decode(token, options={"verify_signature": False}) # works in PyJWT >= v2.0
+#print (decoded)
 
 
